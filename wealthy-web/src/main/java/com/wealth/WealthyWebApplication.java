16defb339a83a3a33e2484dba79d8a63cb58d696
BrunuusBrazil <brunocardoso.fatec.itu@gmail.com>
2017-03-25 00:20:18 +0100

    initial commit

---
+++ wealthy-web/src/main/java/com/wealth/WealthyWebApplication.java
@@ -0,0 +1,12 @@
+package com.wealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthyWebApplication.class, args);
+	}
+}
