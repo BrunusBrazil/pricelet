827066dbeb5ac3ec305d324ec467cf2f9f466b1b
BrunuusBrazil <brunocardoso.fatec.itu@gmail.com>
2017-03-25 16:24:09 +0100

    common initial commit

---
+++ wealthy-common/src/main/java/com/wealth/WealthyCommonApplication.java
@@ -0,0 +1,12 @@
+package com.wealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthyCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthyCommonApplication.class, args);
+	}
+}
